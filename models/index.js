9527e0917fadbc171e50c6cd2af9341410935274
aliciaroeck16 <aliciamata00@gmail.com>
2020-05-29 17:47:08 -0500

    adds mongodb to models and schemas to trail and city

--- models/index.js
+++ models/index.js
@@ -1,3 +1,22 @@
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/trail-legs';
+
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log("Mongodb connected...");
+}).catch((error) => {
+  console.log(error);
+});
+  
+mongoose.connection.on('disconnected', () => {
+  console.log("Mongodb disconnected...");
+});
+
 module.exports = {
     City: require('/City'),
     Trail: require('/Trail'),
