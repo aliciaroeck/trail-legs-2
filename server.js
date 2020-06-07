4476e6742bd7e87da130e7cced0961c4ff30825e
aliciaroeck16 <aliciamata00@gmail.com>
2020-06-06 23:13:45 -0500

    resolves most issues with cities index

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.get("/", (req,res) => {
             console.log(err);
             return res.send({message: "Internal Server Error"});
           } else {
-            console.log(req.session);
             const context = 
             {cities: allCities,
             user: req.session.currentUser};
